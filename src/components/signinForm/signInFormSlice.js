4e049b99011ec6297bc1599d660448f259f0414c
MouawiyyaShafiq <mouawiyya.sh@hotmail.com>
2025-04-28 14:43:36 +0200

    removed email and password from global state

--- src/components/signinForm/signInFormSlice.js
+++ src/components/signinForm/signInFormSlice.js
@@ -8,37 +8,17 @@ export const loginUser = createAsyncThunk("signInForm/loginUser",
 const signInFormSlice = createSlice({
     name : "signInForm",
     initialState : {
-        email : "",
-        password : "",
-        error : null,
         token : sessionStorage.getItem("authToken")?sessionStorage.getItem("authToken"):null
     },
     reducers : {
-        inputedEmail : (state , action) => {
-            state.email = action.payload
-        },
-        inputedPassword : (state , action) => {
-            state.password = action.payload
-        },
         resetSignInForm : (state) => {
-            state.error = null
-            state.email = ""
-            state.password = ""
-            state.token = sessionStorage.getItem("authToken")?sessionStorage.getItem("authToken"):null
+            state.token = null
         }
     },
     extraReducers : (builder) => {
         builder.addCase(loginUser.fulfilled, (state,action) => {
-            state.email=""
-            state.password=""
-            state.error=null
             state.token = action.payload
             sessionStorage.setItem("authToken",action.payload)
-        }),
-        builder.addCase(loginUser.rejected, (state,action) => {
-            state.error = action.payload
-            state.email=""
-            state.password=""
         })
     }
 })
